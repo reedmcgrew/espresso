5bae6132a34d69fefbfaead528ba279d3636ff12
Reed McGrew <reed@familylink.com>
2012-02-26 03:02:29 +0000

    Removed project-specific code from the migrations.

--- src/migrations/up.sql
+++ src/migrations/up.sql
@@ -11,14 +11,6 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `eslocators` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(40) NOT NULL,
-  `esl` varchar(400),
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 #USERS
 create user 'lab2_user'@'%' identified by 'silly pass';
 grant all on lab2_user_data.* to 'lab2_user'@'%';
